dcfdae9f09b81e4e021883d7841e7ebd27047c7f
gofunerrors <gofunerrors@gmail.com>
2023-03-07 19:58:39 -0600

    i added second

--- main.go
+++ main.go
@@ -4,7 +4,9 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 */
 package main
 
-import "github.com/golangast/contribute/cmd"
+import (
+	"github.com/golangast/contribute/cmd"
+)
 
 func main() {
 	cmd.Execute()
